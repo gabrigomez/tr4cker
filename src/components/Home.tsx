35b01c6af016a73a0b6c6bf7d5ceecb39ff241f4
Gabriel <gabrigomez15@gmail.com>
2023-06-09 11:12:58 -0300

    feat: wip in basic api call from home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 
 export const Home = () => {
+  const API_URL = "https://python-app.up.railway.app/api"
+  
+  useEffect(() => {
+    const response = axios.get(API_URL)
+    console.log(response)
+  })
+
   return (
     <div>
       Home Component
