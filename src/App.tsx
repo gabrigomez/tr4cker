533d6f66e65d2066fb6d0e464e305f3b35c3ad97
Gabriel <gabrigomez15@gmail.com>
2023-06-09 10:55:54 -0300

    feat: start navbar component and rename main page on index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Routes, Route } from 'react-router'
 import { Home } from './components/Home'
+import { NavBar } from './components/NavBar'
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>       
