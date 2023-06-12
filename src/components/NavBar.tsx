c2f00df683ca23d5937ba85a53c48790c38b642c
Gabriel <gabrigomez15@gmail.com>
2023-06-12 10:48:05 -0300

    feat: add some styles in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <div className='bg-black w-screen'>
-      <p>NavBar</p>
+    <div className='bg-black w-screen mb-4 p-4 border border-sky-900'>
+      <div className='flex justify-between md:justify-around'>
+        <div>
+          <Link to="/" className='hover:text-sky-700 duration-300 cursor-pointer'>
+            TR4CKER
+          </Link>
+        </div>
+        <div className='flex'>
+          <div className='mr-4'>
+            <a className='hover:text-sky-700 duration-300 cursor-pointer'>
+              Login
+            </a>
+          </div>
+          <div>
+            <a className='hover:text-sky-700 duration-300 cursor-pointer'>
+              Sign in
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
