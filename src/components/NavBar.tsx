d3311d623f5c78d129984a9c7141ee2fe678ace3
Gabriel <gabrigomez15@gmail.com>
2023-06-15 11:54:13 -0300

    feat: add conditional renders in navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,15 @@
-import { Keyhole, SignIn } from '@phosphor-icons/react';
-import { Link } from 'react-router-dom';
+import { Keyhole, SignOut, UserCircle, UserCirclePlus } from '@phosphor-icons/react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const NavBar = () => {
+  const token = localStorage.getItem("token")
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    localStorage.removeItem("token")
+    navigate("/login")
+  }
+
   return (
     <div className='bg-black w-screen mb-4 p-4 border border-sky-900'>
       <div className='flex justify-between md:justify-around'>
@@ -11,18 +19,37 @@ export const NavBar = () => {
           </Link>
         </div>
         <div className='flex'>
-          <div className='mr-4 flex items-center group'>
-            <Keyhole className='text-2xl mr-1 group-hover:text-sky-700 cursor-pointer' />
-            <Link to="/login" className='group-hover:text-sky-700 duration-300 cursor-pointer'>
-              Login
-            </Link>
-          </div>
-          <div className='mr-4 flex items-center group'>
-            <SignIn className='text-2xl mr-1 group-hover:text-sky-700 cursor-pointer' />
-            <Link to="/sign-in" className='group-hover:text-sky-700 duration-300 cursor-pointer'>
-              Sign in
-            </Link>
-          </div>
+          {token ? (
+            <div className='flex'>
+              <div className='mr-4 flex items-center group'>
+                <UserCircle className='text-2xl mr-1 group-hover:text-sky-700 cursor-pointer' />
+                <Link to="/dashboard" className='group-hover:text-sky-700 duration-300 cursor-pointer'>
+                  Dashboard
+                </Link>
+              </div>
+              <div className='mr-4 flex items-center group'>
+                <SignOut className='text-2xl mr-1 group-hover:text-sky-700 cursor-pointer' />
+                <button onClick={handleLogout} className='group-hover:text-sky-700 duration-300 cursor-pointer'>
+                  Log out
+                </button>
+              </div>
+            </div>            
+          ) : (
+            <div className='flex'>
+              <div className='mr-4 flex items-center group'>
+                <Keyhole className='text-2xl mr-1 group-hover:text-sky-700 cursor-pointer' />
+                <Link to="/login" className='group-hover:text-sky-700 duration-300 cursor-pointer'>
+                  Login
+                </Link>
+              </div>
+              <div className='mr-4 flex items-center group'>
+                <UserCirclePlus className='text-2xl mr-1 group-hover:text-sky-700 cursor-pointer' />
+                <Link to="/sign-in" className='group-hover:text-sky-700 duration-300 cursor-pointer'>
+                  Sign in
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
