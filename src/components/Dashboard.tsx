2c46d840610f6d240850f27362917230333443ac
Gabriel <gabrigomez15@gmail.com>
2023-06-15 11:07:08 -0300

    feat: add redirect in dashboard when user is not logged

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router"
+
 export const Dashboard = () => {
+  const token = localStorage.getItem("token")
+  const navigate = useNavigate()
+    
+  useEffect(() => {
+    if (!token) {
+      navigate("/login")
+    }
+  })
+  
+
   return (
     <div className="w-screen">
       Dashboard Component
