8bc3dedea306c51bd37ee657d3487282a9b7ab11
Gabriel <gabrigomez15@gmail.com>
2023-06-29 11:42:45 -0300

    fix: disable tracker for unauthenticated users

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Field, Form } from "react-final-form"
 import { API_URL, validateFormValues } from "../utils";
 import { Binoculars } from "@phosphor-icons/react";
 
 import * as yup from "yup";
 import axios from "axios";
+import AuthContext from "../context/AuthContext";
 
 export const Home = () => {
   const [songs, setSongs] = useState<Array<string>>([])
   const [image, setImage] = useState<string>('')
   const [errors] = useState<string>('')  
+  const { authToken } = useContext(AuthContext)
+
   const initialValues = {
     artist: '',
   }
@@ -34,66 +37,68 @@ export const Home = () => {
           TR4CKER
         </p>
       </div>
-      <div className="mt-8">
-        <div className="mb-4">
-          <p>
-            Search for artist
-          </p>
-        </div>
-        <Form
-          onSubmit={onSubmit}
-          validate={validate}
-          render={({ handleSubmit }) => (
-          <form action="submit" className="flex flex-col items-center" onSubmit={handleSubmit}>
-            <Field name="artist">
-              {({ input, meta }) => (
-                <div className="mb-3 flex flex-col w-3/4 md:w-2/4 lg:w-1/4 h-14">
-                  <input 
-                    type="text"
-                    {...input} 
-                    placeholder="ex: Black Sabbath" 
-                    className="bg-black p-2 rounded-md"
-                    />
-                  {meta.error && meta.touched && 
-                    <span className="text-xs text-red-600 mt-1">
-                      {meta.error}
-                    </span>
-                  }
-                </div>
-              )}
-            </Field>            
-            <button className="bg-black hover:bg-gray-900 duration-300 p-2 rounded-md group">
-              <Binoculars className='text-2xl mr-1 cursor-pointer' />
-            </button>
-            {songs.length > 0 && (
-              <div className="w-3/4 md:w-2/4 flex flex-col justify-center items-center mt-4 bg-black p-4 rounded-md">
-                {image !== '' && (
-                  <div className="mb-4 rounded-sm border border-sky-800">
-                    <img src={image} alt="" className="h-44 w-44" />
+      {authToken && (
+        <div className="mt-8">
+          <div className="mb-4">
+            <p>
+              Search for artist
+            </p>
+          </div>
+          <Form
+            onSubmit={onSubmit}
+            validate={validate}
+            render={({ handleSubmit }) => (
+            <form action="submit" className="flex flex-col items-center" onSubmit={handleSubmit}>
+              <Field name="artist">
+                {({ input, meta }) => (
+                  <div className="mb-3 flex flex-col w-3/4 md:w-2/4 lg:w-1/4 h-14">
+                    <input 
+                      type="text"
+                      {...input} 
+                      placeholder="ex: Black Sabbath" 
+                      className="bg-black p-2 rounded-md"
+                      />
+                    {meta.error && meta.touched && 
+                      <span className="text-xs text-red-600 mt-1">
+                        {meta.error}
+                      </span>
+                    }
                   </div>
                 )}
-                <div>
-                  {songs.map((song) => {
-                    return (
-                      <p className="mb-1" key={song}>
-                        {song}
-                      </p>
-                    )
-                  })}
+              </Field>            
+              <button className="bg-black hover:bg-gray-900 duration-300 p-2 rounded-md group">
+                <Binoculars className='text-2xl mr-1 cursor-pointer' />
+              </button>
+              {songs.length > 0 && (
+                <div className="w-3/4 md:w-2/4 flex flex-col justify-center items-center mt-4 bg-black p-4 rounded-md">
+                  {image !== '' && (
+                    <div className="mb-4 rounded-sm border border-sky-800">
+                      <img src={image} alt="" className="h-44 w-44" />
+                    </div>
+                  )}
+                  <div>
+                    {songs.map((song) => {
+                      return (
+                        <p className="mb-1" key={song}>
+                          {song}
+                        </p>
+                      )
+                    })}
+                  </div>
                 </div>
-              </div>
+              )}
+              {errors && (
+                <div className="h-10 text-xs text-red-600 mt-2 font-semibold">
+                  <p>
+                    {errors}
+                  </p>
+                </div>
+              )}
+            </form>
             )}
-            {errors && (
-              <div className="h-10 text-xs text-red-600 mt-2 font-semibold">
-                <p>
-                  {errors}
-                </p>
-              </div>
-            )}
-          </form>
-          )}
-        />
-      </div>
+          />
+        </div>
+      )}      
     </div>
   )
 }
