8900c6565ba31180cbe8b2dc84b0cd9c8693adf4
Gabriel <gabrigomez15@gmail.com>
2023-06-29 11:07:28 -0300

    feat: show top musics and artist image in home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,6 +7,8 @@ import * as yup from "yup";
 import axios from "axios";
 
 export const Home = () => {
+  const [songs, setSongs] = useState<Array<string>>([])
+  const [image, setImage] = useState<string>('')
   const [errors, setErrors] = useState<string>('')  
   const initialValues = {
     artist: '',
@@ -18,6 +20,8 @@ export const Home = () => {
 
   const onSubmit = async(values = { ...initialValues }) => {
     const response = await axios.post(`${API_URL}/spotify`, {...values})
+    setSongs(response.data[1])
+    setImage(response.data[0].images[0].url)
     console.log(response)
   }
 
@@ -30,7 +34,7 @@ export const Home = () => {
           TR4CKER
         </p>
       </div>
-      <div className="mt-10">
+      <div className="mt-8">
         <div className="mb-4">
           <p>
             Search for artist
@@ -61,6 +65,24 @@ export const Home = () => {
             <button className="bg-black hover:bg-gray-900 duration-300 p-2 rounded-md group">
               <Binoculars className='text-2xl mr-1 cursor-pointer' />
             </button>
+            {songs.length > 0 && (
+              <div className="w-3/4 md:w-2/4 flex flex-col justify-center items-center mt-4 bg-black p-4 rounded-md">
+                {image !== '' && (
+                  <div className="mb-4 rounded-sm border border-sky-800">
+                    <img src={image} alt="" className="h-44 w-44" />
+                  </div>
+                )}
+                <div>
+                  {songs.map((song) => {
+                    return (
+                      <p className="mb-1" key={song}>
+                        {song}
+                      </p>
+                    )
+                  })}
+                </div>
+              </div>
+            )}
             {errors && (
               <div className="h-10 text-xs text-red-600 mt-2 font-semibold">
                 <p>
