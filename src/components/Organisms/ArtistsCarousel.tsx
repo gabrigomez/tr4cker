9c89d548b4c95bdf7fa43a34428f555df53f343e
Gabriel <gabrigomez15@gmail.com>
2023-08-08 11:30:04 -0300

    styles: fix shadows in Artist Image

--- src/components/Organisms/ArtistsCarousel.tsx
+++ src/components/Organisms/ArtistsCarousel.tsx
@@ -13,7 +13,7 @@ export const ArtistsCarousel = ({imgArray} : ArtistsCarouselProps) => {
       showThumbs={false}
       showIndicators={false}
       showStatus={false}
-      className='w-4/5 h-4/5 sm:w-2/4 sm:h-2/4 lg:w-2/6 lg:h-2/6 rounded-full overflow-hidden mb-2 border-2 border-pink-800'
+      className='w-4/5 h-4/5 sm:w-2/4 sm:h-2/4 lg:w-2/6 lg:h-2/6 rounded-full overflow-hidden border border-pink-900 mb-2 shadow-pink-900 shadow-md'
     >
       {imgArray.map((img) => {
         return (
